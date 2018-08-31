c9ebf540d0faeec554812aef713396529f86fd94
dyygs <dy1525471395@live.com>
2018-08-31 11:01:10 +0800

    ajax请求完成及展示

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
 import React, {Component} from 'react';
 import './App.css';
-import http from './server/server'
+import $ from 'jquery';
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {provinceName: '', citys: []};
+        this.state = {
+            provinceName: '',
+            citys: []
+        };
+        this.getCitys = this.getCitys.bind(this);
     }
 
-    async getCitys(provinceName) {
+    getCitys() {
+        // console.log(this.state.provinceName);
+        // $.get('http://192.168.6.202:8080/city/getCity?provinceName=%E6%B2%B3%E5%8D%97%E7%9C%81', function(result) {
+        //     if (this.isMounted) {
+        //         this.setState({
+        //             provinceName: this.state.provinceName,
+        //             citys: result[0].data
+        //         });
+        //     }
+        // }.bind(this));
+        const _this = this;
+        $.ajax({
+            type:'get',
+            // url:'http://localhost:8080/city/getCity?provinceName=河南省',
+            url:'http://localhost:8080/city/getCity?provinceName=' + this.state.provinceName,
+            // dataType:'jsonp',
+            success:function(res){
+                _this.setState({
+                    citys: res
+                });
+                console.log(_this.state.provinceName);
+            },
+            error:function(result){
+                alert("error" + result);
+            }
+        })
+    }
 
-        const res = await http.get('/city/getCity', {provinceName:provinceName})
-        console.log(res.data);
+    handelChange(e){
         this.setState({
-            provinceName: provinceName,
-            citys: res.data
-        });
-    }
-
-    handleChange(inputValue) {
-        this.setState({
-            provinceName: inputValue
-        });
-    }
-
-
-    componentDidMount() {
-        this.getCitys();
+            provinceName:e.target.value
+        })
     }
 
     render() {
-        var inputValue = '';
         return (
             <div>
-                <h1>省份：{this.state.provinceName}</h1>
-                <input type="text"/>
-                <button onClick={this.getCitys(this.state.provinceName)}>查看包含市:</button>
+                <h1>根据输入的省份查看包含的市</h1>
+                <input type="text" onChange={this.handelChange.bind(this)} defaultValue={this.state.provinceName}/>
+                <button onClick={this.getCitys}>查看包含市:</button>
                 <ul>
                     {
                         this.state.citys.map((city,index) => {
+                            console.log("i am refresh");
+                            var obj = eval(city);
                             return (
-                                <li key={index}>{city.cityName}</li>
+                                <li key={index}>{obj.cityName}</li>
                             )
                         })
                     }
